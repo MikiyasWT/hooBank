d3445cabcb3b485e7c338bc91779eeb6cae9f02a
mikiyaswt <mikiywendu44@gmail.com>
2022-10-19 14:04:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './style'
+import { Billing,Business,Button,CarDeal,Clients,CTA,FeedbackCard,Footer,GetStarted,Hero,NavBar,Stats,Testimonials} from './components'
+
+
+function App() {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+
+    </div>
+  )
+}
+
+export default App
