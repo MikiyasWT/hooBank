b38fccb661172918461d0b76995eafe4a9d1f6dc
mikiyaswt <mikiywendu44@gmail.com>
2022-10-23 21:19:19 +0300

    card deal done

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {apple,bill, google} from '../assets'
+import styles, { layout } from '../style'
+
 
 function Billing() {
   return (
-    <div>Billing</div>
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="bill" className='w-[100%] h-[100%] relative z-[5]' />
+        <div className='absolute z-[3] top-0 -left-1/2 w-[50%] h-[50%] rounded-full white__gradient'/>
+        <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient'/>
+      </div>
+
+     <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Easily control your <br className="sm:block hidden"/> billing & invoicing.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      For a dealer to purchase from Coaster, it must have an account with Coaster and a
+      Customer Number. The Customer Number must be associated.
+      </p>
+      <div className='flex flex-row flex-wrap sm:mt-10 mt-6'>
+      <img src={apple} alt="apple_store" className='w-[124px] h-[64px] cursor-pointer object-contain mr-5'/>
+      <img src={google} alt="apple_store" className='w-[124px] h-[64px] cursor-pointer object-contain'/>
+      </div>
+     </div> 
+    </section>
   )
 }
 
--- src/components/CarDeal.jsx
+++ src/components/CarDeal.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { card } from '../assets'
+import styles, { layout } from '../style'
+import Button from './Button'
+
 
 const CarDeal = () => {
   return (
-    <div>CarDeal</div>
+    <section className='flex flex-col sm:flex-row'>
+     <div className={`flex flex-col ${layout.sectionInfo}`}>
+     layout.sectionInfo
+      <h2 className={`${styles.heading2}`}>Find a better Card deal <br className='sm:block hidden'/> in few easy steps</h2>
+      <p className={`${styles.paragraph} text-dimWhite`}>
+      For a dealer to purchase from Coaster, it must have an account with Coaster and a
+      Customer Number. The Customer Number must be associated.
+      </p>
+      <Button styles="mt-10" />
+     </div>
+
+     <div className={`flex flex-col ${layout.sectionImg}`}>
+      <img src={card} alt="card" className="w-[472px] h-[280px]" />
+     </div>
+    </section>
+
   )
 }
 
