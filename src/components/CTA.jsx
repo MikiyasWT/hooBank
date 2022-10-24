1a0bb587cb1e6a3c7a863dee045f0440a699ebde
mikiyaswt <mikiywendu44@gmail.com>
2022-10-24 16:35:56 +0300

    click to action done

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import styles from '../style'
+import {Button} from '../components'
 //call to action
 
 
 function CTA() {
   return (
-    <div>CTA</div>
+   <section>
+    <div className='left-12 right-12 py-10 px-10 bg-black-gradient flex flex-col justify-between rounded-[16px] sm:flex-row'>
+        <div className='flex flex-col'>
+        <h1 className={`${styles.heading2}`}>
+          Let's try our Service now
+        </h1>
+        <p className={`${styles.paragraph}`}>
+        Everything you need to accept card payments <br className='sm:block hidden'/>and grow your business anywhere on the planet.
+        </p>
+        </div>
+
+        <div className='flex justify-start sm:items-center'>
+          <Button styles="mt-2 p-2 rounded-[10px] "/>
+        </div>
+    </div>
+   </section>
+
   )
 }
 
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -4,7 +4,7 @@ import {clients} from '../constants'
 import styles from '../style'
 const Company = ({logo}) => (
      <div className={`${styles.flexCenter } flex-wrap w-full min-w-[120px] sm:min-w-[192px] `}>
-      <img  src={logo} alt="logo" className='sm:w-[192px] w-[100px] object-contain '/>
+      <img  src={logo} alt="logo" className=' hover:grayscale grayscale-0 sm:w-[192px] w-[100px] object-contain '/>
      </div>
 )
 
